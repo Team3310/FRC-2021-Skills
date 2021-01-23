427cb414abb5899fc64a12410340a1b09c08ab3e
BrianSelle <bselle@cebridge.net>
2021-01-23 07:39:00 -0600

    Revert "Changed from WPI source to Jack in the bots"
    This reverts commit 3db5c73e980b5fa96210dc3ede5979fff31719a7.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final class DriveConstants {
+    public static final int kFrontLeftDriveMotorPort = 14;
+    public static final int kRearLeftDriveMotorPort = 15;
+    public static final int kFrontRightDriveMotorPort = 1;
+    public static final int kRearRightDriveMotorPort = 0;
+
+    public static final int kFrontLeftTurningMotorPort = 10;
+    public static final int kRearLeftTurningMotorPort = 11;
+    public static final int kFrontRightTurningMotorPort = 5;
+    public static final int kRearRightTurningMotorPort = 4;
+
+    public static final int kGyroPort = 16;
+
+    public static final double TURN_OUTPUT_TO_ENCODER_RATIO = 12.8;
+    public static final double DRIVE_OUTPUT_TO_ENCODER_RATIO = 8.16;
+    public static final double ENCODER_TICKS_PER_MOTOR_REVOLUTION = 2048.0;
+    public static final double kWheelDiameterInches = 4.0;
+
+    // Distance between centers of right and left wheels on robot
+    public static final double kTrackWidth = 0.34;
+
+    // Distance between front and back wheels on robot
+    public static final double kWheelBase = 0.34;
+
+    public static final SwerveDriveKinematics kDriveKinematics =
+        new SwerveDriveKinematics(
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+    // These characterization values MUST be determined either experimentally or theoretically
+    // for *your* robot's drive.
+    // The RobotPy Characterization Toolsuite provides a convenient tool for obtaining these
+    // values for your robot.
+    public static final double ksVolts = 1;
+    public static final double kvVoltSecondsPerMeter = 0.8;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.15;
+
+    public static final double kMaxSpeedMetersPerSecond = 9;
+  }
+
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 9;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 7;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profilied robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+        new TrapezoidProfile.Constraints(
+            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,234 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+@SuppressWarnings("PMD.ExcessiveImports")
+public class DriveSubsystem extends SubsystemBase {
+  // Robot swerve modules
+  private final SwerveModuleFalcon m_frontLeft =
+      new SwerveModuleFalcon(
+          DriveConstants.kFrontLeftDriveMotorPort,
+          DriveConstants.kFrontLeftTurningMotorPort,
+              true);
+
+  private final SwerveModuleFalcon m_frontRight =
+      new SwerveModuleFalcon(
+          DriveConstants.kFrontRightDriveMotorPort,
+          DriveConstants.kFrontRightTurningMotorPort,
+              false);
+
+  private final SwerveModuleFalcon m_rearLeft =
+      new SwerveModuleFalcon(
+          DriveConstants.kRearLeftDriveMotorPort,
+          DriveConstants.kRearLeftTurningMotorPort,
+              true);
+    
+  private final SwerveModuleFalcon m_rearRight =
+      new SwerveModuleFalcon(
+          DriveConstants.kRearRightDriveMotorPort,
+          DriveConstants.kRearRightTurningMotorPort,
+              false);
+
+  private boolean isDriveEnabled = false;
+  private double xSpeed;
+  private double ySpeed;
+  private double rot;
+  private double speedCommandLF;
+  private double speedCommandRF;
+  private double speedCommandLB;
+  private double speedCommandRB;
+  private double rotCommandLF;
+  private double rotCommandRF;
+  private double rotCommandLB;
+  private double rotCommandRB;
+
+  // The gyro sensor
+  private PigeonIMU m_gyro = new PigeonIMU(DriveConstants.kGyroPort);
+  private double[] xyz_dps = new double[3];
+
+  // Odometry class for tracking robot pose
+  SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(DriveConstants.kDriveKinematics, Rotation2d.fromDegrees(m_gyro.getFusedHeading()));
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    m_gyro.configFactoryDefault();
+    resetHeading();
+    resetEncoders();
+  }
+
+  @Override
+  public void periodic() {
+      // Update the odometry in the periodic block
+    m_odometry.update(
+        Rotation2d.fromDegrees(getHeading()),
+        m_frontLeft.getState(),
+        m_frontRight.getState(),
+        m_rearLeft.getState(),
+        m_rearRight.getState()
+    );
+      
+    SmartDashboard.putNumber("Heading deg", getHeading());
+    SmartDashboard.putNumber("Heading Rate deg per s", getTurnRate());
+    SmartDashboard.putNumber("Front Right Angle deg", m_frontRight.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Front Left Angle deg", m_frontLeft.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Rear Right Angle deg", m_rearRight.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Rear Left Angle deg", m_rearLeft.getState().angle.getDegrees());
+    SmartDashboard.putNumber("Front Right Speed m per s", m_rearRight.getState().speedMetersPerSecond);
+    SmartDashboard.putNumber("xSpeed", xSpeed);
+    SmartDashboard.putNumber("ySpeed", ySpeed);
+    SmartDashboard.putNumber("rot", rot);
+    SmartDashboard.putNumber("Left Front Speed", speedCommandLF);
+    SmartDashboard.putNumber("Left Front Angle", rotCommandLF);
+    SmartDashboard.putNumber("Left back Speed", speedCommandLB);
+    SmartDashboard.putNumber("Left back Angle", rotCommandLB);
+    SmartDashboard.putNumber("Right Front Speed", speedCommandRF);
+    SmartDashboard.putNumber("Right Front Angle", rotCommandRF);
+    SmartDashboard.putNumber("Right Back Speed", speedCommandRB);
+    SmartDashboard.putNumber("Right Back Angle", rotCommandRB);
+
+  }
+
+  public void setTurnAngle(double angleDegrees) {
+    m_frontRight.setTurnAngle(angleDegrees);
+    m_frontLeft.setTurnAngle(angleDegrees);
+    m_rearRight.setTurnAngle(angleDegrees);
+    m_rearLeft.setTurnAngle(angleDegrees);
+  }
+
+  public void setWheelSpeed(double metersPerSec) {
+    m_frontRight.setWheelSpeed(metersPerSec);
+    m_frontLeft.setWheelSpeed(metersPerSec);
+    m_rearRight.setWheelSpeed(metersPerSec);
+    m_rearLeft.setWheelSpeed(metersPerSec);
+  }
+  public void setWheelSpeedPercent(double percent) {
+    m_frontRight.setWheelSpeed(percent);
+    m_frontLeft.setWheelSpeed(percent);
+    m_rearRight.setWheelSpeed(percent);
+    m_rearLeft.setWheelSpeed(percent);
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    m_odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  @SuppressWarnings("ParameterName")
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.rot = rot;
+    if (isDriveEnabled) {
+      var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+          fieldRelative
+              ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, Rotation2d.fromDegrees(getHeading()))
+              : new ChassisSpeeds(xSpeed, ySpeed, rot));
+      SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+      speedCommandLF = swerveModuleStates[0].speedMetersPerSecond;
+      rotCommandLF = swerveModuleStates[0].angle.getDegrees();
+      speedCommandLB = swerveModuleStates[2].speedMetersPerSecond;
+      rotCommandLB = swerveModuleStates[2].angle.getDegrees();
+      speedCommandRF = swerveModuleStates[1].speedMetersPerSecond;
+      rotCommandRF = swerveModuleStates[1].angle.getDegrees();
+      speedCommandRB = swerveModuleStates[3].speedMetersPerSecond;
+      rotCommandRB = swerveModuleStates[3].angle.getDegrees();
+
+      m_frontLeft.setDesiredState(swerveModuleStates[0]);
+      m_frontRight.setDesiredState(swerveModuleStates[1]);
+      m_rearLeft.setDesiredState(swerveModuleStates[2]);
+      m_rearRight.setDesiredState(swerveModuleStates[3]);
+    } 
+  }
+
+  public void setDriveEnabled(boolean isEnabled) {
+    isDriveEnabled = isEnabled;
+    if (!isEnabled) {
+      m_frontLeft.setControlOff();
+      m_frontRight.setControlOff();
+      m_rearLeft.setControlOff();
+      m_rearRight.setControlOff();
+    }
+  }
+
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param desiredStates The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.normalizeWheelSpeeds(desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(desiredStates[0]);
+    m_frontRight.setDesiredState(desiredStates[1]);
+    m_rearLeft.setDesiredState(desiredStates[2]);
+    m_rearRight.setDesiredState(desiredStates[3]);
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    m_frontLeft.resetEncoders();
+    m_frontRight.resetEncoders();
+    m_rearLeft.resetEncoders();
+    m_rearRight.resetEncoders();
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void resetHeading() {
+    m_gyro.setYaw(0);
+    m_gyro.setFusedHeading(0);
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return m_gyro.getFusedHeading();
+  }
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    m_gyro.getRawGyro(xyz_dps);
+    return xyz_dps[0];
+  }
+}
